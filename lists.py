9d82bec1c4933d8a6029175c7b53b23cc901fe2f
Klas Olsson <klas.olsson@memoratus.se>
2021-12-12 15:54:52 +0100

    initial commit

---
+++ lists.py
@@ -0,0 +1,8 @@
+pizza={
+        'crust':'thick',
+        'toppings': ['cheese','onion','tomato sauce',
+            'olives','tuna','pepperoni'],
+        }
+print(pizza)
+print(pizza['crust'])
+
---
+++ var.js
@@ -0,0 +1,5 @@
+// taking variables for a spin
+let x
+x = 10
+x *= 2
+console.log(x)
